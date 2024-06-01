bbb06a71985a037d4750a9f4699e1fd4eef8dbba
TDMVu18 <rinputin482@gmai.com>
2024-06-01 23:14:06 +0700

    feat: text to speech module

--- ui/services/whisper_handler/text2speech.py
+++ ui/services/whisper_handler/text2speech.py
@@ -1,2 +1,26 @@
-def text2speech (response):
-    pass
+from gtts import gTTS
+from pydub import AudioSegment
+import pygame
+import os
+
+def text2speech(pandasResponse):
+    language = "en"
+    text_to_speak = pandasResponse
+
+    # Create a cache sound file to execute TTS
+    gtts_object = gTTS(text=text_to_speak, lang=language, slow=False)
+    gtts_object.save("cache_sound.mp3")
+
+    # Convert mp3 to wav AudioSegment
+    audio = AudioSegment.from_mp3("cache_sound.mp3")
+    audio.export("cache_sound.wav", format="wav")
+
+    # Just load wav file converted from mp3 cache
+    pygame.mixer.init()
+    pygame.mixer.music.load("cache_sound.wav")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    # Remove cache mp3 file
+    os.remove("cache_sound.mp3")
