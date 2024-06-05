ccb0d0f4383e860bc8fa2e9cba90dfe68a242fc9
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2024-06-06 00:32:14 +0700

    fix: ignore

--- ui/constants/const.py
+++
@@ -1,8 +0,0 @@
-execute_whisper = False
-
-def set_execute_whisper(value):
-    global execute_whisper
-    execute_whisper = value
-
-def get_execute_whisper():
-    return execute_whisper
--- ui/pages/landingpage.py
+++ ui/pages/landingpage.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-import time
 import whisper
 
-from constants.const import set_execute_whisper, get_execute_whisper
+from constants.global_varient import set_execute_whisper, get_execute_whisper
 from components.course_grid import QnA_SQL, display_course_grid
 from services.rasa_api import get_rasa_response
 from services.intent_handlers.intent_classify import intent_classification, whisper_intent_classification
