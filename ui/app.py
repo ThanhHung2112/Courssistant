49aecd7b9faf6748002d7bda71d028b35831f6c4
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2024-06-06 21:38:54 +0700

    revert: store

--- ui/app.py
+++ ui/app.py
@@ -3,29 +3,28 @@ import pandas as pd
 import time
 import whisper
 
+from constants.global_varient import set_df_display, get_df_display
 from components.course_grid import display_course_grid
 from services.whisper_handler.speech2text import get_speech_input
 from services.chat.chat_histories import save_chat_history, load_chat_history
 from services.chat.intent_pipeline import process_user_input
-
+# from components.sidebar import sidebar
 import warnings
 
 # Suppress the warning about st.experimental_get_query_params being deprecated
 warnings.filterwarnings("ignore", message=".*st.experimental_get_query_params.*", category=FutureWarning)
 
 st.set_page_config(layout="wide")
-from constants.global_varient import set_df_display, get_df_display
-import warnings
+
 
 # Suppress the warning about st.experimental_get_query_params being deprecated
 warnings.filterwarnings("ignore", message=".*st.experimental_get_query_params.*", category=FutureWarning)
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
 
-df = pd.read_csv("assistant/data/courssistant_main.csv")
 # Initialize or load chat history
 if "messages" not in st.session_state:
     st.session_state.messages = load_chat_history()
@@ -38,11 +37,12 @@ if "is_listening" not in st.session_state:
 if "whisper_model" not in st.session_state:
     st.session_state.whisper_model = whisper.load_model("base")
 
+
 #----------------------------------------------
 # SIDEBAR
 # Sidebar with a button to delete chat history
 with st.sidebar:
-    if st.button("Delete Chat History"):
+    if st.button("Delete Chat History", key="app"):
         st.session_state.messages = []
         save_chat_history([])
 
@@ -63,8 +63,6 @@ with st.sidebar:
         user_input = st.chat_input("How can I help?", key="user_input")
         if user_input:
             df = process_user_input(chat_container, user_input)
-
-            df = process_user_input(chat_container, user_input)
             set_df_display(df)
         # Add microphone button
         mic_button = st.button("🎤", key="mic_button")
@@ -74,15 +72,13 @@ with st.sidebar:
                 user_input = get_speech_input()
                 if user_input:
                     df = process_user_input(chat_container, user_input)
-
+                    set_df_display(df)
 #----------------------------------------------
 # MAIN PAGE
-st.title("Online Courses")
 st.title("Online Courses")
 zone = st.empty()
 current = time.time()
 with zone.container():
     print(f"Finished QnA in {time.time() - current} seconds")
-    
     df = get_df_display()
     display_course_grid(df[:100])
